16c4cd8eb347a478947440be3ae94b4d3b500c52
tomwey <tomwey@163.com>
2018-04-27 14:22:24 +0800

    ccc

--- app/admin/device_info.rb
+++ app/admin/device_info.rb
@@ -1,4 +1,12 @@
 ActiveAdmin.register DeviceInfo do
+  active_admin_import validate: true,
+            template_object: ActiveAdminImport::Model.new(
+                hint: "文件导入格式为: '主板','品牌','CPU','CPU2','驱动','显示','指纹','硬件','产品型号','生产商','型号','产品','启动引导','HOST','编译标记','Incremental'",
+                csv_headers: ["board","brand","cpu","cpu2","device","display","fingerprint","hardware","product_model","manufacturer","model","product","bootloader","host","build_tags","incremental"],
+                force_encoding: :auto
+            ),
+            back: {action: :index}
+  
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
