1d6775a6935d1b5bc1bb54286ca79913531e1b79
tomwey <tomwey@163.com>
2018-05-07 23:24:51 +0800

    add rake task

--- lib/spider_008.rb
+++ lib/spider_008.rb
@@ -4,7 +4,34 @@ class Spider
   def self.start
     r = RestClient.post 'http://api.008shenqi.com/api/xtools/x008/device/v1', {time: Time.now.to_i, 
       id: 27266, imei: '714254171108722', ' MANUFACTURER': 'Huawei'}
+    
     puts r.body
+    # res = JSON.parse(r.body)
+    #
+    # model = res["model"]
+    # if model.present?
+    #   count = DeviceInfo.where(model: model).count
+    #   if count == 0
+    #     DeviceInfo.create!(board: res["board"],
+    #                        brand: res["brand"],
+    #                        cpu: res["setCpuName"],
+    #                        cpu2: res["arch"],
+    #                        device: res["device"],
+    #                        display: "",
+    #                        fingerprint: res["fingerprint"],
+    #                        hardware: res["hardware"],
+    #                        product_model: "",
+    #                        manufacturer: res["manufacturer"],
+    #                        model: model,
+    #                        product: res["product"],
+    #                        bootloader: res["bootloader"],
+    #                        host: "",
+    #                        build_tags: "",
+    #                        incremental: ""
+    #                        )
+    #   end
+    # end
+    #
   end
 end
 
