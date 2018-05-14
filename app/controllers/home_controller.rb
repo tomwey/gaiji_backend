1cc9b20b6136f22ede3c34735850b3fe045803b6
tomwey <tomwey@163.com>
2018-05-14 22:40:45 +0800

    ccc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,17 +8,21 @@ class HomeController < ApplicationController
   end
   
   def app_start
-    @tasks = AwakeTask.where(opened: true).where('task_count != 0 and task_count > complete_count').order('sort desc, id desc')
+    @tasks = NewTask.where(opened: true, task_type: 1).where('task_count != 0 and task_count > complete_count').order('sort desc, id desc')
   end
   
   def upload_awake_task_log
     task_id = params[:task_id]
-    task = AwakeTask.find_by(uniq_id: task_id)
+    task = NewTask.find_by(uniq_id: task_id)
     
     if task.blank?
       re = 4004
     else
-      AwakeTaskLog.create!(task_id: task.uniq_id, proj_id: task.project.try(:uniq_id), awake_source: params[:source])
+      # TODO: 每次取最新的一条数据进行操作，因为默认是先改机再进行APP唤醒
+      log = NewTaskLog.where(task_id: task.uniq_id).order('created_at desc').first
+      log.extra_data = params[:source]
+      log.save!
+      # AwakeTaskLog.create!(task_id: task.uniq_id, proj_id: task.project.try(:uniq_id), awake_source: params[:source])
       re = 1
     end
     render text: re
