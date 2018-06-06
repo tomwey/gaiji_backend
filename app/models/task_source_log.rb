b065cc88c32d7f5e24eff0c625a4170119f366b3
tomwey <tomwey@163.com>
2018-06-06 10:04:37 +0800

    ccc

--- app/models/task_source_log.rb
+++ app/models/task_source_log.rb
@@ -1,2 +1,17 @@
 class TaskSourceLog < ActiveRecord::Base
+  
+  def self.to_csv
+    attributes = %w{id extra_data}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |log|
+        csv << attributes.map{ |attr| log.send(attr) }
+      end
+      
+    end
+    
+  end
+    
 end
