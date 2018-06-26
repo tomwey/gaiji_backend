0b3b53bf412111fa6934231024682c97a38ea86f
tomwey <tomwey@163.com>
2018-06-26 13:34:14 +0800

    add some func

--- lib/api/v1/datasource_api.rb
+++ lib/api/v1/datasource_api.rb
@@ -4,11 +4,38 @@ module API
     class DatasourceAPI < Grape::API
       resource :ds, desc: '身份证信息，昵称，评论，聊天数据源接口' do
         desc "获取一条身份证信息"
+        params do
+          optional :flag, type: Integer, desc: '是否获取唯一的身份证信息，值为0或1，0表示不一定唯一，1表示获取一条唯一的身份证信息'
+          optional :cl, type: Integer, desc: '是否清空历史'
+        end
         get :idcard do
-          @idcard = Idcard.order('RANDOM()').first
+          flag = (params[:flag] || 0).to_i
+          
+          unless %w(0 1).include? flag.to_s
+            return render_error(-1, 'flag参数不正确，只能为0或1')
+          end
+          
+          queued = $redis.get 'queued'
+          puts queued
+          if flag == 1
+            @idcard = Idcard.where.not(card_no: queued).order('RANDOM()').first
+          else
+            @idcard = Idcard.order('RANDOM()').first
+          end
+          
           if @idcard.blank?
             return render_error(4004, '无数据')
           end
+          
+          if flag == 1
+            queued << @idcard.card_no
+            $redis.set 'queued', queued
+          end
+          
+          if params[:cl] && params[:cl] == 1
+            $redis.del 'queued'
+          end
+          
           { code: 0, message: 'ok', data: {
             name: @idcard.name,
             idcard: @idcard.card_no
