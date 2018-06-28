3e4b22abf51a224ede2c288d3727beabdbc6c627
tomwey <tomwey@163.com>
2018-06-28 10:21:34 +0800

    ccc

--- app/admin/task_source_log.rb
+++ app/admin/task_source_log.rb
@@ -12,10 +12,28 @@ ActiveAdmin.register TaskSourceLog do
 #   permitted
 # end
 
-  controller do
-    def scoped_collection
-      end_of_association_chain.where(visible: true)
-    end
+action_item only: :index do
+  link_to '清空昨天的数据', action: 'clear_data'
+end
+
+collection_action :clear_data, method: :get do
+  date = (Time.zone.now - 1.days).strftime('%Y-%m-%d')
+  TaskSourceLog.where('date(created_at) = ?', date).update_all(visible: false)
+  redirect_to collection_path, notice: "清空成功！"
+end
+
+controller do
+  def scoped_collection
+    end_of_association_chain.where(visible: true)
   end
+  
+  # def clear_data
+  #   date = (Time.zone.now - 1.days).strftime('%Y-%m-%d')
+  #   TaskSourceLog.where('date(created_at) = ?', date).update_all(visible: false)
+  #   redirect_to collection_path, notice: "清空成功！"
+  # end
+end
+
+
 
 end
