4d9be5105c936e4ded0fb61cb393ede085f88e56
tomwey <tomwey@163.com>
2018-07-31 21:44:01 +0800

    ccc

--- app/admin/new_task.rb
+++ app/admin/new_task.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register NewTask do
   
   menu parent: 'rom_menu', priority: 4, label: '刷量任务'
 
-  permit_params :proj_id, :task_count, :task_date, :opened, :task_type, :portal_urls_val
+  permit_params :proj_id, :task_count, :task_date, :opened, :task_type, :portal_urls_val, :remain_ratios
 
   index do
     selectable_column
@@ -47,6 +47,7 @@ ActiveAdmin.register NewTask do
       f.input :proj_id, as: :select, label: '所属项目', collection: Project.where(opened: true).order('created_at desc').map { |p| [p.name, p.uniq_id] }, prompt: '-- 选择项目 --', required: true
       f.input :task_count, placeholder: '任务量'
       f.input :task_type, as: :select, label: '任务类型', collection: NewTask::TASK_TYPEs, required: true
+      f.input :remain_ratios, label: '任务留存率', placeholder: '多天留存率使用英文逗号分隔,例如：50,30,10'
       f.input :task_date, as: :string, placeholder: '例如：2018-01-10', hint: '任务开始日期'
       f.input :opened, label: '是否开启'
       f.input :portal_urls_val, as: :text, label: '该任务入口地址', placeholder: '多个地址请用英文逗号(,)或竖线(|)分隔'
